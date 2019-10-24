aaa3bae6edb305e4e5a6aaf3d68766bd6a830567
DanielCaminos <danicaminos@hotmail.com>
2019-10-23 21:00:39 -0300

    userController brings filtered users

---
+++ src/application/controllers/userController.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+
+
+const URL ='https://2eja2nqth0.execute-api.us-east-1.amazonaws.com/api/users';
+const options = { "method": "GET" };
+
+const getUrl = (url, method) => {
+  return fetch(url, method)
+    .then(res => res.json())
+    .catch(err => {
+      console.error(`Error ${err}`);
+    });
+};
+let userController = {
+    getAll: async (req, res) => {
+      const data = await getUrl(URL, options); 
+      const users = data.users ; 
+      const aux = users.filter(d => d.is_active = true);
+      aux.sort(function (a, b) {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+        return 0;
+      })         
+   // for(i=0;i<aux.length;i++){
+     //   Object.defineProperty(aux[i],'Edad',{value:10});       
+       // }
+     res.json(aux);
+    }};
+module.exports =userController;
--- src/application/routes/index.js
+++ src/application/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = new express.Router();
+const userController = require('../controllers/userController')
 
 const HelloWorldCtrl = new (require('../controllers/helloWorld.controller'))();
 
-router
-	.route('/hello-world')
-	.get(HelloWorldCtrl.sayHi);
+router.route('/hello-world').get(HelloWorldCtrl.sayHi);
+
+
+router.get('/users', userController.getAll);
 
 module.exports = router;
