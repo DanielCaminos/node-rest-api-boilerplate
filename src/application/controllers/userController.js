14e3e3162a2b0893b32032053b9b8512b081e2e9
DanielCaminos <danicaminos@hotmail.com>
2019-10-23 21:32:02 -0300

    placed age attribute

--- src/application/controllers/userController.js
+++ src/application/controllers/userController.js
@@ -1,33 +1,40 @@
 const fetch = require('node-fetch');
 
 
-const URL ='https://2eja2nqth0.execute-api.us-east-1.amazonaws.com/api/users';
-const options = { "method": "GET" };
+const URL = 'https://2eja2nqth0.execute-api.us-east-1.amazonaws.com/api/users';
+const options = { method: 'GET' };
 
-const getUrl = (url, method) => {
-  return fetch(url, method)
-    .then(res => res.json())
-    .catch(err => {
-      console.error(`Error ${err}`);
+const getUrl = (url, method) => fetch(url, method)
+  .then((res) => res.json())
+  .catch((err) => {
+    console.error(`Error ${err}`);
+  });
+const userController = {
+  getAll: async (req, res) => {
+    const data = await getUrl(URL, options);
+    const { users } = data;
+    const aux = users.filter((d) => d.is_active = true);
+    aux.sort((a, b) => {
+      if (a.lastname > b.lastname) {
+        return 1;
+      }
+      if (a.lastname < b.lastname) {
+        return -1;
+      }
+      return 0;
     });
+    aux.forEach((a) => {
+        const birthday = a.birthday.substring(0, 4);
+        const currentYear = new Date().getFullYear();
+        const age = currentYear - birthday;
+
+        a.age = age;
+      });
+
+    // for(i=0;i<aux.length;i++){
+    //   Object.defineProperty(aux[i],'Edad',{value:10});
+    // }
+    res.json(aux);
+  },
 };
-let userController = {
-    getAll: async (req, res) => {
-      const data = await getUrl(URL, options); 
-      const users = data.users ; 
-      const aux = users.filter(d => d.is_active = true);
-      aux.sort(function (a, b) {
-        if (a.lastname > b.lastname) {
-          return 1;
-        }
-        if (a.lastname < b.lastname) {
-          return -1;
-        }
-        return 0;
-      })         
-   // for(i=0;i<aux.length;i++){
-     //   Object.defineProperty(aux[i],'Edad',{value:10});       
-       // }
-     res.json(aux);
-    }};
-module.exports =userController;
+module.exports = userController;
--- src/application/routes/index.js
+++ src/application/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+
 const router = new express.Router();
-const userController = require('../controllers/userController')
+const userController = require('../controllers/userController');
 
 const HelloWorldCtrl = new (require('../controllers/helloWorld.controller'))();
 
